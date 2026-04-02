4c1957abcddf0c30a5d160b049a51c47d08db84e
GordonBie123 <gordonbie123@gmail.com>
2026-04-01 23:51:28 -0400

    Updated intro

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -11,9 +11,9 @@ export const profile = {
   aboutBio: [
     "I’m <strong>Gordon Bie</strong>, a third-year <strong>Information Systems and International Business, Data Science Minor</strong> student at <strong>Northeastern University</strong>. My path to tech was unconventional—moving from international relations to business before discovering programming, which immediately hooked me.",
     "I’ve developed a strong interest in <strong>Machine Learning</strong> and <strong>AI</strong>. I’m currently a <strong>Tech Lead working on an AI software tool at Cortex Ventures</strong>, and previously interned at <strong>State Street</strong> as an AI Engineer and Data Analyst. Additionally, I have worked multiple research assistant jobs and will be interning at <strong>Flow Traders</strong> as a Trading Engineer.",
-    "Originally from <strong>Vancouver, BC</strong> and now based in <strong>Boston, MA</strong>, I enjoy cooking, gaming (board games and video games), matcha, and discovering new music outside of class and work."
+    "Originally from <strong>Vancouver, BC</strong> and now based in <strong>Boston, MA</strong> and will be based in <strong>New York, NY</strong>. I enjoy cooking, gaming (board games and video games), matcha, and discovering new music outside of class and work."
   ],
-  shortBio: "I'm a business major who wants to become an engineer. I have SWE, AI, and ML research experience, read more about it below!",
+  shortBio: "I'm a business major who learned how to code. I have SWE, AI, and ML research experience, read more about it below!",
   contactTitle: "Get In Touch",
   contactDescription: "Let's collaborate or just chat about data science, AI, or literally anything else!",
   contactStatus: "Currently open to research and anything else that is cool",
